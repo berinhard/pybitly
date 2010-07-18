26e02b33bed20df3566115a9d2ca7f65e0ca09f6
Bernardo Fontes <bernardoxhc@gmail.com>
2010-07-17 23:09:17 -0300

    Add httplib2 dependency

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="1.0",
     packages=find_packages(),
     scripts = ['pybitly'],
+    install_requires = ['httplib2'],
     author="Bernardo Fontes",
     author_email="falecomigo@bernardofontes.net",
     long_description="The objectve of this project is to create a client to the Bit.ly API. It already exists a Python project to serve as such a client but there is no command line tool. It's just a Python module that you can use it to consume the API inside your project. Besides that, I'm using this project to practise, get more confident with Python programming and learn more about it.",
